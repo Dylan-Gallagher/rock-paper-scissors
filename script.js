a6488a678d74cfd5ef3a48e2ee2bfd3290740c37
Dylan Gallagher <1dylangallagher@gmail.com>
2021-07-12 14:14:09 +0100

    New UI

--- script.js
+++ script.js
@@ -18,10 +18,73 @@ function computerPlay() {
     }
 }
 
-button.onclick = function() {
-    let userHand = input.value;
-    console.log(userHand);
+const r = document.querySelector('.rock');
+const p = document.querySelector('.paper');
+const s = document.querySelector('.scissors');
+
+let userHand;
+
+r.onclick = function() {
+    userHand = 'rock';
+
     computerPlay();
+
+    if (computerHand == 'rock' && userHand == 'paper') {
+        para.textContent = "Computer chose rock, you win";
+    } else if (computerHand == 'paper' && userHand == 'rock') {
+        para.textContent = "Computer chose paper, you lose"
+    } else if (computerHand == 'rock' && userHand == 'scissors') {
+        para.textContent = "Computer chose rock, you lose"
+    } else if (computerHand == 'scissors' && userHand == 'rock') {
+        para.textContent = "Computer chose scissors, you win"
+    } else if (computerHand == 'scissors' && userHand == 'paper') {
+        para.textContent = "Computer chose scissors, you lose"
+    } else if (computerHand == 'paper' && userHand == 'scissors') {
+        para.textContent = "Computer chose paper, you win"
+    } else if (computerHand == 'paper' && userHand == 'paper') {
+        para.textContent = "Computer chose paper, it's a tie!"
+    } else if (computerHand == 'rock' && userHand == 'rock') {
+        para.textContent = "Computer chose rock, it's a tie!"
+    } else if (computerHand == 'scissors' && userHand == 'scissors') {
+        para.textContent = "Computer chose scissors, it's a tie!"
+    } else
+        para.textContent = "Please enter rock, paper, or scissors"
+}
+
+
+p.onclick = function() {
+    userHand = 'paper';
+
+    computerPlay();
+
+    if (computerHand == 'rock' && userHand == 'paper') {
+        para.textContent = "Computer chose rock, you win";
+    } else if (computerHand == 'paper' && userHand == 'rock') {
+        para.textContent = "Computer chose paper, you lose"
+    } else if (computerHand == 'rock' && userHand == 'scissors') {
+        para.textContent = "Computer chose rock, you lose"
+    } else if (computerHand == 'scissors' && userHand == 'rock') {
+        para.textContent = "Computer chose scissors, you win"
+    } else if (computerHand == 'scissors' && userHand == 'paper') {
+        para.textContent = "Computer chose scissors, you lose"
+    } else if (computerHand == 'paper' && userHand == 'scissors') {
+        para.textContent = "Computer chose paper, you win"
+    } else if (computerHand == 'paper' && userHand == 'paper') {
+        para.textContent = "Computer chose paper, it's a tie!"
+    } else if (computerHand == 'rock' && userHand == 'rock') {
+        para.textContent = "Computer chose rock, it's a tie!"
+    } else if (computerHand == 'scissors' && userHand == 'scissors') {
+        para.textContent = "Computer chose scissors, it's a tie!"
+    } else
+        para.textContent = "Please enter rock, paper, or scissors"
+}
+
+
+s.onclick = function() {
+    userHand = 'scissors';
+
+    computerPlay();
+
     if (computerHand == 'rock' && userHand == 'paper') {
         para.textContent = "Computer chose rock, you win";
     } else if (computerHand == 'paper' && userHand == 'rock') {
